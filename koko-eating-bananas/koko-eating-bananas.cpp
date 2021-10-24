68cfddb35b016a84eff05f37c4ba0ecca62d7099
Mayank Goyal <67274027+rexa0310@users.noreply.github.com>
2021-10-24 11:34:31 +0530

    Time: 40 ms (87.48%), Space: 18.8 MB (80.58%) - LeetHub

---
+++ koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& v, int m)
+    {
+        if(v.size()==1)
+            return v[0]%m?v[0]/m+1:v[0]/m;
+        int maxi=INT_MIN,mini=INT_MAX;
+     for(auto x:v)
+         maxi=max(maxi,x);
+        long long l=1,h=maxi;
+      // cout<<l<<" "<<h;
+        while(l<h)
+        {
+            long long mid=l+(h-l)/2;
+            long long c=0;
+            for(auto x:v)
+            {
+                c+=x/mid;
+                if(x%mid)
+                    c++;
+                
+            }
+           cout<<c<<" "<<mid<<endl;
+            if(c>m)
+                l=mid+1;
+            else
+                h=mid;
+        }
+        return h;
+    }
+};
