60b22f72debe4e8e48bc6555a5ae8422e6f8d4f6
Mayank Goyal <67274027+rexa0310@users.noreply.github.com>
2022-03-27 11:32:24 +0530

    Time: 282 ms (14.29%), Space: 121.2 MB (57.14%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -4,14 +4,8 @@ public:
          int c = 0;
             for (int i = 0; i < nums.size() - 1; i++)
             {
-                if ((i + c) % 2 == 0)
-                {
-                    if (nums[i] == nums[i + 1])
-                    {
+                if ((i + c) % 2 == 0 && nums[i] == nums[i + 1])
                         c++;
-                    }
-                }
-                cout<<c<<" ";
             }
         if((nums.size()-c)%2==1)
         c++;
