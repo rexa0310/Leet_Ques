b13d3fb297a095b3e08a5e9fe8f31e9d50001da1
Mayank Goyal <67274027+rexa0310@users.noreply.github.com>
2022-04-01 12:31:28 +0530

    Time: 789 ms (5.15%), Space: 178.2 MB (5.15%) - LeetHub

--- 664-strange-printer/664-strange-printer.cpp
+++ 664-strange-printer/664-strange-printer.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
+     int dp[100][100]={0};
     int strangePrinter(string s) {
-      int dp[100][100]={0};
+     
         
         int n = s.size();
-        return operate(s, 0, n-1,dp);   
+        return operate(s, 0, n-1);   
     }
 
-    int operate(string s, int i, int j,int dp[100][100])
+    int operate(string s, int i, int j)
     {
      
         if (i>j) 
@@ -15,11 +16,11 @@ public:
        if(dp[i][j]>0)
            return dp[i][j];
            
-        int res =1+operate(s, i+1, j,dp);
+        int res =1+operate(s, i+1, j);
         for (int m=i+1; m<=j; m++)
         {    
             if (s[i]==s[m])
-        res = min( res, operate(s, i+1, m-1,dp)+operate(s, m, j,dp));
+        res = min( res, operate(s, i+1, m-1)+operate(s, m, j));
         }
           dp[i][j]=res;
         return res;
