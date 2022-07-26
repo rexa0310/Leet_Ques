dbfde969dc66f975dbb04ca7593ae80dc08005d2
Mayank Goyal <67274027+rexa0310@users.noreply.github.com>
2022-07-26 23:31:58 +0530

    Time: 1042 ms (46.59%), Space: 214 MB (23.03%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
    bool dfs(vector<vector<int>>& g, vector<int>& vis, int x, int ds) {
-        if(x == ds)
-            return true;
-        if(vis[x])
-            return false;
-        
-        vis[x] = 1;
-        
-        for(int i=0; i<g[x].size(); i++)
-            if(dfs(g, vis, g[x][i], ds))
-                return true;
-        
-        
-        return false;
+      if(x==ds)
+          return true;
+       
+       if(vis[x])
+           return false;
+       vis[x]=1;
+       for(auto it:g[x])
+           if(!vis[it] and dfs(g,vis,it,ds))
+               return true;
+       return false;
     }
     
     bool validPath(int n, vector<vector<int>>& edges, int start, int ds) 
